7fed23a879d21c59d427d0bb881eb40dc3acca6e
Muhammad AKbarul Ihsan <barullisan@gmail.com>
2023-05-30 20:40:49 +0700

    fix: bug

---
+++ app/tes/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React, { useRef, useEffect, useState } from 'react';
+
+const Page = () => {
+
+  return (
+    <div className='card'>
+    <div className="flex ">
+  <div className="w-1/2">
+    {/* Content for the left half of the card */}
+  </div>
+  <div className="w-1/2">
+    {/* Content for the right half of the card */}
+  </div>
+</div>
+</div>
+    );
+};
+
+export default Page;
